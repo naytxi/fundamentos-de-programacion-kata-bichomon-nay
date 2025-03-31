d8fd30c58a9429c700b3abbdb7ee8c0f752be905
Nahia Otegui <nay.fullstack@gmail.com>
2025-03-31 19:53:32 +0200

    script bichomon

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 console.log(document.title);
+
+ const title = document.getElementById('gen-1');
+ title.textContent="Generasión 1 Pokimon";
+
+ const pokimon = document.getElementsByClassName("infocard");
+ for (let i = 0; i < 151 && i < pokimon.length; i++) {
+    pokimon[i].style.backgroundColor = "orange";
+}
+
+console.log(window.location.href);
+console.log(document.domain);
+
+let images = document.querySelectorAll("img");
+console.log(images);
+
+images.forEach(function(img){
+    img.src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+});
+
+const info = document.querySelectorAll(".infocard-lg-data.text-muted .itype.flying");
+info.forEach( function(element) {
+    element.style.backgroundColor = "yellow";
+});
+
+
+
